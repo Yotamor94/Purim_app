cd885a0dd151d7e606219b540455ec1d1d645869
Yotamor94 <oren.yotam@gmail.com>
2019-02-23 19:05:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/purim_app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.purim_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void IMadeAchange(){
+        String hello = "hello";
+    }
+}
